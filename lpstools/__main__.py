5111247ef5769ef2d9dfa65b69489a50f043e71e
Brian Raymond <brian.dionigi@gmail.com>
2018-07-03 11:26:40 -0700

    Revert "Init"
    This reverts commit 06423f10c60070628e23fb0aae7f04a93caf1c10.

--- lpstools/__main__.py
+++
@@ -1,5 +0,0 @@
-from lpstools.gui import main
-
-# Running the gui by default.
-if __name__ == '__main__':
-    main()
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-from setup_common import setup_options
-
-setup(extras_require={
-    'pyqt5': ['pyqt5']
-},
-    zip_safe=False,
-    **setup_options)
